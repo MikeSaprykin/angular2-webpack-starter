c81be57e84eb007161aa8859519dffb2957173db
mikesaprykin <mikesaprykin@gmail.com>
2016-07-09 17:37:45 +0300

    first commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by Mike on 09.07.16.
+ */
+import {
+  it,
+  inject,
+  describe,
+  beforeEachProviders,
+  expect
+} from '@angular/core/testing';
+import { AppComponent } from './app.component';
+describe('App', () => {
+  beforeEachProviders(() => [
+    AppComponent
+  ]);
+  it ('should work', inject([AppComponent], (app: AppComponent) => {
+    // Add real test here
+    expect(2).toBe(2);
+  }));
+});
---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import '../public/css/style.css';
+@Component({
+  selector: 'my-app',
+  templateUrl: 'src/app/app.component.html',
+  styleUrls: ['src/app/app.component.css']
+})
+export class AppComponent { }
